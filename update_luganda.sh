6f012ee799a25724ae8cde81d44ba975c2f92172
omd <muhumuzaomega@gmail.com>
2025-04-30 07:17:14 +0300

    Edit (30/04/2025)

--- update_luganda.sh
+++ update_luganda.sh
@@ -336,17 +336,17 @@ DEST1="$HOME/Omd/Exercise/Luganda"
 SRC2="$HOME/Omd/Students/Omd/Revision/Luganda"
 DEST2="$HOME/Omd/Revision/Luganda"
 if ! find "$DEST1" -type f -name "*.ans.txt" | grep -q .; then
-  cp -r "$SRC1"/* "$DEST1"
+  cp -r "$SRC1"/. "$DEST1" > /dev/null 2>&1
 fi
 if find "$DEST2" -type f -name "*.qns.txt" | grep -q .; then
   read -rp $'\n\nExisting exercise and revision files have been detected.\n\nUpdating them now may overwrite your current progress.\n\nIf you want to continue and overwrite, type "yes" or "y".\nOtherwise, press Enter to cancel: ' prompt
   if [[ "$prompt" == "yes" || "$prompt" == "y" ]]; then
-    cp -r "$SRC1"/* "$DEST1"
-    cp -r "$SRC2"/* "$DEST2"
+    cp -r "$SRC1"/. "$DEST1" > /dev/null 2>&1
+    cp -r "$SRC2"/. "$DEST2" > /dev/null 2>&1
     echo -e "\nExercise and revision files successfully updated!"
   else
     echo -e "\nUpdate cancelled. Existing exercise and revision files remain untouched!"
   fi
 else
-  cp -r "$SRC2"/* "$DEST2"
+  cp -r "$SRC2"/. "$DEST2" > /dev/null 2>&1
 fi
--- update_physics.sh
+++ update_physics.sh
@@ -365,17 +365,17 @@ DEST1="$HOME/Omd/Exercise/Physics"
 SRC2="$HOME/Omd/Students/Omd/Revision/Physics"
 DEST2="$HOME/Omd/Revision/Physics"
 if ! find "$DEST1" -type f -name "*.ans.txt" | grep -q .; then
-  cp -r "$SRC1"/* "$DEST1"
+  cp -r "$SRC1"/. "$DEST1" > /dev/null 2>&1
 fi
 if find "$DEST2" -type f -name "*.qns.txt" | grep -q .; then
   read -rp $'\n\nExisting exercise and revision files have been detected.\n\nUpdating them now may overwrite your current progress.\n\nIf you want to continue and overwrite, type "yes" or "y".\nOtherwise, press Enter to cancel: ' prompt
   if [[ "$prompt" == "yes" || "$prompt" == "y" ]]; then
-    cp -r "$SRC1"/* "$DEST1"
-    cp -r "$SRC2"/* "$DEST2"
+    cp -r "$SRC1"/. "$DEST1" > /dev/null 2>&1
+    cp -r "$SRC2"/. "$DEST2" > /dev/null 2>&1
     echo -e "\nExercise and revision files successfully updated!"
   else
     echo -e "\nUpdate cancelled. Existing exercise and revision files remain untouched!"
   fi
 else
-  cp -r "$SRC2"/* "$DEST2"
+  cp -r "$SRC2"/. "$DEST2" > /dev/null 2>&1
 fi
